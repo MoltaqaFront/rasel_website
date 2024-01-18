5efcff84e43c2c3f3ddc1706993a48f0585b33d0
mosbah@moltaqa.net <74296680+Mosbah-Mohamed@users.noreply.github.com>
2024-01-18 12:50:26 +0200

    1st upload

---
+++ locales/en-US.js
@@ -0,0 +1,66 @@
+export default {
+  lang: 'ltr',
+
+  nav: {
+    home: 'HOME',
+    about: 'ABOUT APP',
+    screen: 'APP SCREENS',
+    download: 'DOWNLOAD NOW',
+    contact: 'CONTACT US',
+    terms: 'TERMS & CONDITIONS',
+    policy: 'Privacy Policy',
+    backToHome: 'BACK TO HOME',
+    lang: 'ع',
+  },
+  registerLink: 'REGISTER AS DRIVER',
+  mainSlider: [
+    {
+      mainTitle: 'About Rasel',
+      title: 'Rasel: "Call, Organize, Remember!"',
+      desc: 'It reflects our commitment to facilitate communication, organize daily life, and remind users of important events. The Raseel app is your reliable partner for time management and effective communication. Join us today and enjoy the efficiency and ease of organizing your life.',
+    },
+  ],
+  feature: [
+    {
+      image: '1.svg',
+      title: 'A Constant Companion for Users',
+      desc: 'Our mission is to be the constant companion for users in managing and organizing their lives. We strive to achieve this by providing a user-friendly and innovative interface that allows users to create and manage alerts easily.',
+    },
+    {
+      image: '2.svg',
+      title: 'A Comfortable User Experience',
+      desc: 'Our goals are to improve the user experience and provide high-quality service. We continuously work on developing and enhancing the application, meeting the needs and aspirations of current and future users.',
+    },
+    {
+      image: '3.svg',
+      title: 'Join Raseel',
+      desc: 'We invite you to download the Raseel app and join our growing community of users interested in communication and reminders. Discover the ease of communication, organization, and reminders with the Raseel app, and enjoy the new experience that will change the way you interact with events and appointments in your life.',
+    },
+  ],
+  about: {
+    mainTitle: 'Rasel app is your ideal choice',
+    title: 'About Rasel',
+    desc: '<p>A multi-service online store application that allows users to purchase products and services online and have them delivered to their address. The application also includes reservation options for restaurants and events, and facilitates the process of obtaining the required services in an easy and convenient way. Those who are looking for a modern and sophisticated shopping experience, as it provides many distinctive and convenient services that make online shopping easy and enjoyable. Therefore, the application can be downloaded easily and benefit from all the services available on it with comfort and confidence. Get the best online shopping experience with the Pring application. Enjoy competitive prices and high quality of the products you buy through the Rasel app. Fast and reliable delivery for all your purchases, with secure and easy payment options</p>',
+  },
+  downloadText:
+    '<p>You can download the Rasel app now by visiting the App Store on your smartphone, searching for Rasel, and then downloading the app on your smartphone.</p>',
+  isoLink: '',
+  androidLink: '',
+  address: 'Kingdom of Saudi Arabia - Jizan',
+  terms: {
+    title: 'Terms And Conditions',
+  },
+  policy: {
+    title: 'Privacy Policy',
+  },
+  copyWrite: '©2023 All Rights Reserved For Moltaqa Software House',
+
+  contact: {
+    title: 'Contact Us',
+    name: 'name',
+    email: 'e-mail',
+    phone: 'phone',
+    message: 'message',
+    send: 'send ',
+  },
+}
